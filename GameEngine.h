7ecb4e81c2be4afd67f1d2b199c466912412bb59
G3tLUCky <56052135+G3tLUCky@users.noreply.github.com>
2019-10-29 13:27:22 +0100

    Update GameEngine.h

--- GameEngine.h
+++ GameEngine.h
@@ -1,185 +1,155 @@
-/*
- * GameEngine.h
- *
- *  Created on: 1 okt. 2019
- *      Author: Luc Wolfs
- */
-
 #ifndef GAMEENGINE_H_
 #define GAMEENGINE_H_
 
 class GameEngine{
 private:
-	int map[5][5];
-	string mapPath;
+    int map[10][10];
+    string mapPath;
 public:
-	GameEngine(string mapPath);
-	void readMap();
-	void printMap();
-	//char controls(); still experimenting on it
-	void makeMove(int x1, int y1, int x2, int y2);
-	bool isLegalMove(int x1, int y1, int x2, int y2);
-	bool isVerticalLegal(int x1, int y1, int x2, int y2);
-	bool isHorizontalLegal(int x1, int y1, int x2, int y2);
-	bool isSameRow(int x1, int x2);
-	bool isSameColumn(int y1, int y2);
+    GameEngine(string mapPath);
+    void readMap();
+    void printMap();
+    void makeMove(int x1, int y1, int x2, int y2);
+    bool isLegalMove(int x1, int y1, int x2, int y2);
+    bool isAdjecent(int x1, int y1, int x2, int y2);
+    bool isVerticalLegal(int x1, int y1, int x2, int y2);
+    bool isHorizontalLegal(int x1, int y1, int x2, int y2);
+    bool isSameRow(int x1, int x2);
+    bool isSameColumn(int y1, int y2);
+    void gameLoop();
 
 };
 
 GameEngine::GameEngine(string mapPath){
-	this->mapPath = mapPath;
-	readMap();
+    this->mapPath = mapPath;
+    readMap();
 }
 
 void GameEngine::readMap(){
-	ifstream mapFile(mapPath);
+    ifstream mapFile(mapPath);
 
-	int counter = 0;
-	int i=0;
-	int j=0;
+    int counter = 0;
+    int i=0;
+    int j=0;
+    if(mapFile){
+        cout<<"map loaded"<<endl;
+    }
 
-	if(mapFile){
-		cout<<"map loaded"<<endl;
-	}
+    while(mapFile){
+        string line;
+        getline(mapFile, line);
 
-	//just in case there's no file: create a very basic grid
-	else{
-		for(int i=0;i<5;i++){
-			for(int j=0;j<5;j++){
-			map[i][j]=randint(0, 4);
-			}
-		}
-	}
+        j=0; //j resets to 0 after each line
+        for(char c: line){ //for each char in string
+            if(c!=','){
+                map[i][j] = atoi(&c); //char into int
+                j++;
+            }
+        }
+        i++; //next line
 
-	while(mapFile){
-		string line;
-		getline(mapFile, line);
-		if(counter > 0){
-			j=0;
-			for(char c: line){
-				if(c!=','){
-					map[i][j] = atoi(&c);
-					j++;
-				}
-			}
-			i++;
-		}
-		counter++;
-	}
+        counter++;
+    }
 
-
-	printMap();
-
+    printMap();
+    gameLoop();
 
 }
 
 void GameEngine::printMap(){
-	string cursor = "|";//the token on the game board which you're currently at.
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-		if(i==0 and j==0){
-			cout<<cursor<<map[i][j];
-		}
-		else{
-		cout<<" "<<map[i][j];}
-		}
-	cout<<endl;
-	}
+    int height = sizeof (map) / sizeof (*map);
+    int width = sizeof (*map) / sizeof (int);
+    //cout << height << width << endl;
+
+    for(int i=0; i<height; i++){ //ideally this would be mapsize something somwething
+        for(int j=0; j<width; j++){
+            if(j != 9){
+                cout << map[i][j] << " ";
+            } else {
+                cout << map[i][j];
+            }
+        }
+        cout << endl;
+    }
 }
 
 //makeMove changes two elements on the grid
 void GameEngine::makeMove(int x1, int y1, int x2, int y2){
-	int temp = map[x1][y1];
-	map[x1][y1]= map[x2][y2];
-	map[x2][y2]=temp;
+	swap(map[x1][y1],map[x2][y2]);
 }
 
 
 bool GameEngine::isLegalMove(int x1, int y1, int x2, int y2){
-	bool y = isVerticalLegal(x1, y1, x2, y2);
-	bool x = isHorizontalLegal(x1, y1, x2, y2);
-	bool row = isSameRow(x1, x2);
-	bool column = isSameColumn(y1, y2);
+	if(isVerticalLegal(x1, y1, x2, y2) || isHorizontalLegal(x1, y1, x2, y2)){
+			if (isAdjecent(x1,y1, x2, y2)){
+		return true;
+		}
+	}
 
-	if(y and row){
-		return true;
-	}
-	else if(x and column){
-		return true;
-	}
 	return false;
-
-
 }
 
+bool GameEngine::isAdjecent(int x1, int y1, int x2, int y2){
+if (!((x1 == x2 && abs(y1-y2) == 1) || (y1 == y2 && abs(x1-x2) == 1))){
+	//std::cout << "Non-Adjecent move, try again" << std::endl;
+	return false;}
+	else return true;}
 
-bool GameEngine::isVerticalLegal(int x1, int y1, int x2, int y2){
-	if(((map[x2][y2]==map[x1][y1-2]) and (map[x2][y2]==map[x1][y1-1]))
-			or ((map[x1][y1]==map[x2][y2-2]) and(map[x1][y1]==map[x2][y2-1]))){
+bool GameEngine::isVerticalLegal(int x1, int y1, int x2, int y2){ // check before swap is made
+	if(((map[x2][y2]==map[x1][y1-2]) && (map[x2][y2]==map[x1][y1-1]))
+			|| ((map[x1][y1]==map[x2][y2-2]) && (map[x1][y1]==map[x2][y2-1]))){
 		return true;
 	}
-	else if(((map[x2][y2]==map[x1][y1+2]) and (map[x2][y2]==map[x1][y1+1]))
-			or ((map[x1][y1]==map[x2][y2+2]) and(map[x1][y1]==map[x2][y2+1]))){
+	else if(((map[x2][y2]==map[x1][y1+2]) && (map[x2][y2]==map[x1][y1+1]))
+			|| ((map[x1][y1]==map[x2][y2+2]) && (map[x1][y1]==map[x2][y2+1]))){
 		return true;
 	}
-	else if(((map[x2][y2]==map[x1][y1-1]) and (map[x2][y2]==map[x1][y1+1]))
-			or ((map[x1][y1]==map[x2][y2-1]) and(map[x1][y1]==map[x2][y2+1]))){
+	else if(((map[x2][y2]==map[x1][y1-1]) && (map[x2][y2]==map[x1][y1+1]))
+			|| ((map[x1][y1]==map[x2][y2-1]) && (map[x1][y1]==map[x2][y2+1]))){
 		return true;
 	}
 	return false;
 }
 
-bool GameEngine::isHorizontalLegal(int x1, int y1, int x2, int y2){
-	if(((map[x2][y2]==map[x1-2][y1]) and (map[x2][y2]==map[x1-1][y1]))
-			or ((map[x1][y1]==map[x2-2][y2]) and(map[x1][y1]==map[x2-2][y2]))){
+bool GameEngine::isHorizontalLegal(int x1, int y1, int x2, int y2){ // check before swap is made
+	if(((map[x2][y2]==map[x1-2][y1]) && (map[x2][y2]==map[x1-1][y1]))
+			|| ((map[x1][y1]==map[x2-2][y2]) && (map[x1][y1]==map[x2-1][y2]))){
 		return true;
 	}
-	else if(((map[x2][y2]==map[x1+2][y1]) and (map[x2][y2]==map[x1+1][y1]))
-			or ((map[x1][y1]==map[x2+2][y2]) and(map[x1][y1]==map[x2+2][y2]))){
-		return true;
 
-	}
-	else if(((map[x2][y2]==map[x1-1][y1]) and (map[x2][y2]==map[x1+1][y1]))
-			or ((map[x1][y1]==map[x2-1][y2]) and(map[x1][y1]==map[x2][y2]))){
+	else if(((map[x2][y2]==map[x1+2][y1]) && (map[x2][y2]==map[x1+1][y1]))
+			|| ((map[x1][y1]==map[x2+2][y2]) && (map[x1][y1]==map[x2+1][y2]))){
 		return true;
 	}
-	return false;
-}
 
-
-bool GameEngine::isSameRow(int x1, int x2){
-	if(x1==x2){
+	else if(((map[x2][y2]==map[x1-1][y1]) && (map[x2][y2]==map[x1+1][y1]))
+			|| ((map[x1][y1]==map[x2-1][y2]) && (map[x1][y1]==map[x2+1][y2]))){
 		return true;
 	}
 	return false;
 }
 
-bool GameEngine::isSameColumn(int y1, int y2){
-	if(y1==y2){
-		return true;
+void GameEngine::gameLoop(){
+	int x1, y1, x2, y2;
+
+	while(true){
+		cout<<"Give the x-coordinate of the first tile"<<endl;
+		cin>>x1;
+		cout<<"Give the y-coordinate of the first tile"<<endl;
+		cin>>y1;
+
+		cout<<"Give the x-coordinate of the second tile"<<endl;
+		cin>>x2;
+		cout<<"Give the y-coordinate of the second tile"<<endl;
+		cin>>y2;
+		if(isLegalMove(y1, x1, y2, x2)){
+			makeMove(y1, x1, y2, x2);
+		}
+		else{
+			cout<<"Invalid move"<<endl;
+			break;
+		}
+		printMap();
 	}
-	return false;
 }
-
-
-
-
-
-//function for selecting a token on the board. Still in development...
-/*char GameEngine::controls(){
-	char input;
-	cin>>input;
-	if(input=='w'){
-		return input;
-	}
-	if(input=='s'){
-		return input;
-	}
-	if(input=='d'){
-		return input;
-	}
-	if(input=='a'){
-		return input;
-	}
-}*/
 #endif /* GAMEENGINE_H_ */
