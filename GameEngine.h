33806fe29e4aa841ca2a3dca7aa4d6ad2387365a
G3tLUCky <56052135+G3tLUCky@users.noreply.github.com>
2019-11-12 15:10:28 +0100

    Update GameEngine.h

--- GameEngine.h
+++ GameEngine.h
@@ -6,11 +6,13 @@ class GameEngine{
 private:
     int map[10][10];
     string mapPath;
+    string futurePath;
 public:
-    GameEngine(string mapPath);
-    void readMap();
+    GameEngine(string mapPath, string futurePath);
+    void readMap(string mapPath); //+int* ar as parameter when implementing future map
     void printMap();
-    //char controls(); still experimenting on it
+    void readFuture();
+
     void makeMove(int x1, int y1, int x2, int y2);
     bool isLegalMove(int x1, int y1, int x2, int y2);
     bool isAdjecent(int x1, int y1, int x2, int y2);
@@ -18,17 +20,21 @@ public:
     bool isHorizontalLegal(int x1, int y1, int x2, int y2);
     bool isSameRow(int x1, int x2);
     bool isSameColumn(int y1, int y2);
+    bool hasLegalMoves();
+    void gameLoop();
 
 };
 
-GameEngine::GameEngine(string mapPath){
+GameEngine::GameEngine(string mapPath, string futurePath){
     this->mapPath = mapPath;
-    readMap();
+    this->futurePath = futurePath;
+    //readMap(mapPath, map);
+    readMap(mapPath);
+
 }
 
-void GameEngine::readMap(){
+void GameEngine::readMap(string mapPath){//+int* ar as parameters when implementing future map
     ifstream mapFile(mapPath);
-
     int counter = 0;
     int i=0;
     int j=0;
@@ -53,6 +59,7 @@ void GameEngine::readMap(){
     }
 
     printMap();
+    gameLoop();
 
 }
 
@@ -61,7 +68,7 @@ void GameEngine::printMap(){
     int width = sizeof (*map) / sizeof (int);
     //cout << height << width << endl;
 
-    for(int i=height; i>-1; i--){ //ideally this would be mapsize something somwething
+    for(int i=0; i<height; i++){ //ideally this would be mapsize something somwething
         for(int j=0; j<width; j++){
             if(j != 9){
                 cout << map[i][j] << " ";
@@ -80,54 +87,101 @@ void GameEngine::makeMove(int x1, int y1, int x2, int y2){
 
 
 bool GameEngine::isLegalMove(int x1, int y1, int x2, int y2){
-	if(isVerticalLegal(x1, y1, x2, y2) or isHorizontalLegal(x1, y1, x2, y2)){
-			if (isAdjecent(x1,x2,y1,y2)){
+	if(isVerticalLegal(x1, y1, x2, y2) || isHorizontalLegal(x1, y1, x2, y2)){
+			if (isAdjecent(x1,y1, x2, y2)){
 		return true;
 		}
 	}
-	else cout << "No valid move made";
 
 	return false;
 }
 
 bool GameEngine::isAdjecent(int x1, int y1, int x2, int y2){
 if (!((x1 == x2 && abs(y1-y2) == 1) || (y1 == y2 && abs(x1-x2) == 1))){
-	std::cout << "Non-Adjecent move, try again" << std::endl;
+	//std::cout << "Non-Adjecent move, try again" << std::endl;
 	return false;}
 	else return true;}
 
 bool GameEngine::isVerticalLegal(int x1, int y1, int x2, int y2){ // check before swap is made
-	if(((map[x2][y2]==map[x1][y1-2]) and (map[x2][y2]==map[x1][y1-1]))
-			or ((map[x1][y1]==map[x2][y2-2]) and(map[x1][y1]==map[x2][y2-1]))){
+	if(((map[x2][y2]==map[x1][y1-2]) && (map[x2][y2]==map[x1][y1-1]) && y1>1 && y2>y1)
+			|| ((map[x1][y1]==map[x2][y2-2]) && (map[x1][y1]==map[x2][y2-1]) && y2>1 && y1>y2)){
+		//cout<<"1"<<endl;
 		return true;
 	}
-	else if(((map[x2][y2]==map[x1][y1+2]) and (map[x2][y2]==map[x1][y1+1]))
-			or ((map[x1][y1]==map[x2][y2+2]) and(map[x1][y1]==map[x2][y2+1]))){
+	else if(((map[x2][y2]==map[x1][y1+2]) && (map[x2][y2]==map[x1][y1+1]) && y1<8 && y1>y2)
+			|| ((map[x1][y1]==map[x2][y2+2]) && (map[x1][y1]==map[x2][y2+1]) && y2<8 && y2>y1)){
+		//cout<<"2"<<endl;
 		return true;
 	}
-	else if(((map[x2][y2]==map[x1][y1-1]) and (map[x2][y2]==map[x1][y1+1]))
-			or ((map[x1][y1]==map[x2][y2-1]) and(map[x1][y1]==map[x2][y2+1]))){
+	else if(x1!=x2 && (((map[x2][y2]==map[x1][y1-1]) && (map[x2][y2]==map[x1][y1+1]) && (y1>0 && y1<9))
+			|| ((map[x1][y1]==map[x2][y2-1]) && (map[x1][y1]==map[x2][y2+1]) && (y2>0 && y2<9)))){
+		//cout<<"3"<<endl;
 		return true;
 	}
 	return false;
 }
 
 bool GameEngine::isHorizontalLegal(int x1, int y1, int x2, int y2){ // check before swap is made
-	if(((map[x2][y2]==map[x1-2][y1]) and (map[x2][y2]==map[x1-1][y1]))
-			or ((map[x1][y1]==map[x2-2][y2]) and(map[x1][y1]==map[x2-1][y2]))){
+	if(((map[x2][y2]==map[x1-2][y1]) && (map[x2][y2]==map[x1-1][y1]) && x1>1 && x2>x1)
+			|| ((map[x1][y1]==map[x2-2][y2]) && (map[x1][y1]==map[x2-1][y2]) && x2>1 && x1>x2)){
+		//cout<<"4"<<endl;
 		return true;
 	}
 
-	else if(((map[x2][y2]==map[x1+2][y1]) and (map[x2][y2]==map[x1+1][y1]))
-			or ((map[x1][y1]==map[x2+2][y2]) and(map[x1][y1]==map[x2+1][y2]))){
+	else if(((map[x2][y2]==map[x1+2][y1]) && (map[x2][y2]==map[x1+1][y1]) && x1<8 && x1>x2)
+			|| ((map[x1][y1]==map[x2+2][y2]) && (map[x1][y1]==map[x2+1][y2]) && x2<8 && x2>x1)){
+		//cout<<"5"<<endl;
 		return true;
 	}
 
-	else if(((map[x2][y2]==map[x1-1][y1]) and (map[x2][y2]==map[x1+1][y1]))
-			or ((map[x1][y1]==map[x2-1][y2]) and(map[x1][y1]==map[x2+1][y2]))){
+	else if(y1!=y2 &&  (((map[x2][y2]==map[x1-1][y1]) && (map[x2][y2]==map[x1+1][y1]) && (x1>0 && x1<9))
+			|| ((map[x1][y1]==map[x2-1][y2]) && (map[x1][y1]==map[x2+1][y2]) && (x2>0 && x2<9)))){
+		//cout<<"6"<<endl;
 		return true;
 	}
 	return false;
 }
 
+bool GameEngine::hasLegalMoves(){
+	for(int x1=0; x1<9; x1++){
+		for(int y1=0; y1<9; y1++){
+			if(isLegalMove(x1, y1, x1+1, y1) || isLegalMove(x1, y1, x1, y1+1)){
+				//cout<<"found legal move"<<x1<<y1<<endl;
+				return true;
+			}
+		}
+	}
+	cout<<"no legal move found"<<endl;
+	return false;
+}
+
+void GameEngine::gameLoop(){//ask the user for input (x and y have to be a number from 0 to 9)
+	int x1, y1, x2, y2;
+
+	while(hasLegalMoves()){
+		cout<<"Give the x-coordinate of the first tile"<<endl;
+		cin>>x1;
+		cout<<"Give the y-coordinate of the first tile"<<endl;
+		cin>>y1;
+
+		cout<<"Give the x-coordinate of the second tile"<<endl;
+		cin>>x2;
+		cout<<"Give the y-coordinate of the second tile"<<endl;
+		cin>>y2;
+		if(isLegalMove(x1, y1, x2, y2)){
+			makeMove(x1, y1, x2, y2);
+		}
+		else{
+			cout<<"Invalid move"<<endl;
+			break;
+		}
+		printMap();
+	}
+}
+/* TO DO:
+ * -Reading future list
+ * -updateMap()
+ * 	-breaking a row of three
+ *  	-tiles falling down
+ */
 #endif /* GAMEENGINE_H_ */
