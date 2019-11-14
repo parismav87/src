bf070d5a7778e52b8851bcce4c5a3b6b34079c15
WillekeV <56918339+WillekeV@users.noreply.github.com>
2019-11-14 10:36:56 +0100

    Delete common.h

--- common.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-struct Move //struct (sort of mini class) which contains the move coordinates, so that only the move and not the coordinates have to be passed to a function
-{
-	int x1, y1, x2, y2;
-};
